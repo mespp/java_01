b976d14fa28873ded5ae35e1779a6053674eefce
mespp <62350991+mespp@users.noreply.github.com>
2025-10-08 19:29:32 +0200

    cambios menores

--- Pruebas/src/LitrosAGalones.java
+++ Pruebas/src/LitrosAGalones.java
@@ -4,8 +4,8 @@ public class LitrosAGalones {
     public static void main(String[] args) {
         System.out.print("¿Qué cantidad quieres convertir a galones (exprésala en litros)? ");
         Scanner sc = new Scanner(System.in);
-        Double cantidad = sc.nextDouble();
-        Double galones = cantidad * 0.264172;
+        double litros = sc.nextDouble();
+        double galones = litros * 0.264172;
         System.out.println("La cantidad convertida a galones da un total de " + galones + " litros");
     }
 }
